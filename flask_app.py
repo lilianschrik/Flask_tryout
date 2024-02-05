595ea82e3c92775b4a773ddd09b7d5479caa6e63
Lilian <lilianschrik@hotmail.com>
2024-02-05 17:39:42 +0100

    tekst return-statement veranderd

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def home():
-    return '<p>Hello world!</p>'
+    return 'Programmeren voor beginners'
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
